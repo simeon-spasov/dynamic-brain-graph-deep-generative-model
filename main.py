711ed1ebe992130aa68ff4f5359c7927ecbd0901
simeon-spasov <simeon.spsv@gmail.com>
2022-11-24 13:39:27 +0000

    Add model training functionality to main.py and train.py
    Tested by training on 10 fMRI samples. Loss decreases to 0.75 initial ELBO. No posterior collapse observed - KL loss diverges at ˜500 epochs. Tested on 1 and 10 snapshots (latter correspods to window size = 90 and window stride = 40).

--- main.py
+++ main.py
@@ -1,42 +1,55 @@
-import pickle
-import random
+import logging
 
-import numpy as np
 import torch
 
 from src.dataset import load_dataset
 from src.model import Model
 from src.train import train
 
-# setup
-data_dir = "../data"
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-seed = 12345
 
-# hyperparameters
-dataset_args = dict(dataset="hcp", 
-                    window_size=30, 
-                    window_stride=10, 
-                    measure="correlation", 
-                    top_percent=5)
-model_args = dict(sigma=1., 
-                  gamma=0.1, 
-                  categorical_dim=3, 
-                  embedding_dim=32)
-train_args = dict(num_epochs=10001, 
-                  batch_size=25, 
-                  learning_rate=5e-3, 
-                  lamda=100, 
-                  temp_min=0.1, 
-                  anneal_rate=3e-5,
-                  temp=1.)
+def main():
+    logging.basicConfig(
+        filename='fMRI.log',
+        format='%(levelname)s:%(message)s',
+        filemode='w',
+        level=logging.DEBUG)
 
-# dataset
-dataset = load_dataset(**dataset_args, data_dir=data_dir)
-num_subjects, num_nodes = len(dataset), dataset[0][1][0].number_of_nodes()
+    # setup
+    data_dir = "./data"
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    seed = 12345
 
-# model
-model = Model(num_subjects, num_nodes, **model_args, device=device)
+    # hyperparameters
+    dataset_args = dict(dataset="hcp",
+                        window_size=90,
+                        window_stride=40,
+                        measure="correlation",
+                        top_percent=5)
+    model_args = dict(sigma=1.,
+                      gamma=0.1,
+                      categorical_dim=3,
+                      embedding_dim=32)
+    train_args = dict(num_epochs=2001,
+                      batch_size=25,
+                      learning_rate=1e-3,
+                      temp_min=0.1,
+                      anneal_rate=3e-5,
+                      temp=1.)
 
-# train
-model = train(model, dataset, **train_args)
+    # dataset
+    logging.info('Loading data.')
+    dataset = load_dataset(**dataset_args, data_dir=data_dir)
+    experiment_dataset = dataset[:10]
+    num_subjects, num_nodes = len(experiment_dataset), experiment_dataset[0][1][0].number_of_nodes()
+
+    # model
+    model = Model(num_subjects, num_nodes, **model_args, device=device)
+
+    # train
+    logging.info('Starting training.')
+    train(model, experiment_dataset, **train_args)
+    logging.info('Finished script.')
+
+
+if __name__ == '__main__':
+    main()
--- src/dataset.py
+++ src/dataset.py
@@ -1,4 +1,4 @@
-import itertools
+import logging
 import pathlib
 import pickle
 
@@ -11,10 +11,12 @@ def _get_filepaths(dataset="hcp", data_dir="./data", ext=".npy"):
     data_dir = pathlib.Path(data_dir) / dataset / "raw"
     return sorted(list(data_dir.rglob("*" + ext)))
 
+
 def _zscore_timeseries(X, axis=-1):
-    X -= np.mean(X, axis=axis, keepdims=True) 
-    X /= np.std(X, axis=axis, keepdims=True)  
+    X -= np.mean(X, axis=axis, keepdims=True)
+    X /= np.std(X, axis=axis, keepdims=True)
     return X
+
 
 def _load_timeseries(filepath, zscore=False, dtype=np.float64):
     # load multivariate timeseries (nodes x time length)
@@ -25,20 +27,28 @@ def _load_timeseries(filepath, zscore=False, dtype=np.float64):
     y = int(filepath.stem.split("_")[-1])
     return X, y
 
-def _compute_dynamic_fc(X, window_size=30, window_stride=10, measure="correlation", top_percent=5, 
-                        max_time=490, zscore=True, self_loops=False, as_graph=True):
+
+def _compute_dynamic_fc(X,
+                        window_size=30,
+                        window_stride=10,
+                        measure="correlation",
+                        top_percent=5,
+                        max_time=490,
+                        zscore=True,
+                        self_loops=False,
+                        as_graph=True):
     # truncate time length
     time_len = X.shape[-1]
     max_time = max_time if (max_time and max_time <= time_len) else time_len
     X = X[:, :max_time]
-    
+
     if zscore: X = _zscore_timeseries(X)
-    
+
     # calculate starting timepoint for each window
     sampling_points = list(range(0, max_time - window_size, window_stride))
     # initialize functional connectivity measure (uses Ledoit-Wolf covariance estimator)
-    conn_measure = conn.ConnectivityMeasure(kind=measure) 
-    
+    conn_measure = conn.ConnectivityMeasure(kind=measure)
+
     # calculate dynamic functional connectivity within each timeseries window 
     G = []
     for idx in sampling_points:
@@ -47,7 +57,7 @@ def _compute_dynamic_fc(X, window_size=30, window_stride=10, measure="correlatio
         # remove self-loops
         if not self_loops: np.fill_diagonal(A, 0.)
         # calculate percentile threshold value
-        threshold = np.percentile(A.flatten(), 100. - top_percent) 
+        threshold = np.percentile(A.flatten(), 100. - top_percent)
         # threshold 
         A[A < threshold] = 0.
         # return as networkx graph
@@ -55,28 +65,39 @@ def _compute_dynamic_fc(X, window_size=30, window_stride=10, measure="correlatio
 
     return G
 
-def load_dataset(dataset="hcp", window_size=30, window_stride=10, measure="correlation", 
+
+def load_dataset(dataset="hcp", window_size=30, window_stride=10, measure="correlation",
                  top_percent=5, data_dir="../data", **kwargs):
     # load dataset if already exists
     filename = "{}_w-{}_s-{}_m-{}_p-{}.pkl".format(dataset, window_size, window_stride, measure, top_percent)
     _filepath = pathlib.Path(data_dir) / dataset / filename
     if _filepath.exists():
+        logging.info('Found an existing .pkl dataset. Loading...')
         with open(_filepath, "rb") as input_file:
             _dataset = pickle.load(input_file)
     # create and save dataset if does not exist
     else:
+        logging.info('No existing .pkl dataset. Pre-processing fMRI timeseries...')
         _dataset = []
         for idx, filepath in enumerate(_get_filepaths(dataset, data_dir)):
-            X, y  = _load_timeseries(filepath)
+            logging.info(f'Loading timeseries for file {filepath}. Index: {idx}.')
+            X, y = _load_timeseries(filepath)
+            logging.info(f'Computing dynamic functional connectivitiy...')
             G = _compute_dynamic_fc(X, window_size, window_stride, measure, top_percent, **kwargs)
             _dataset += [[idx, G, y]]
         with open(_filepath, "wb") as output_file:
             pickle.dump(_dataset, output_file)
+    logging.info('Loaded dataset.')
     return _dataset
+
 
 def data_loader(dataset, batch_size=10):
     num_samples = len(dataset)
     sample_idx = list(range(num_samples))
-    batch_sample_idx = np.array_split(sample_idx, num_samples // batch_size)
+    if num_samples < batch_size:
+        num_batches = 1
+    else:
+        num_batches = num_samples // batch_size
+    batch_sample_idx = np.array_split(sample_idx, num_batches)
     for batch in batch_sample_idx:
         yield list(map(lambda x: dataset[x], batch))
